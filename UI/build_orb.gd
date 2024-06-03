d1224b4c68c3acbba752de9121f11b09ee665df7
Andrei Rusanov <87578834+ParanormalParrot@users.noreply.github.com>
2024-06-03 15:06:37 +0300

    Update build_orb.gd

--- UI/build_orb.gd
+++ UI/build_orb.gd
@@ -1,4 +1,4 @@
-extends TextureRect
+extends Orb
 
 class_name BuildOrb
 
@@ -7,18 +7,6 @@ class_name BuildOrb
 @onready var dragObjectPrefab = preload("res://UI/tower_drag_object.tscn")
 var currTile
 
-@export var gameController: Node2D
-@export var mana_cost = 100
-
-
-func _ready():
-	$ManaCostLabel.text = str(mana_cost)
-	
-func _process(delta):
-	if gameController.mana >= mana_cost:
-		$ManaCostLabel.set("theme_override_colors/font_color",Color.WHITE)
-	else:
-		$ManaCostLabel.set("theme_override_colors/font_color",Color.RED)
 	
 func _on_gui_input(event):
 	if gameController.mana >= mana_cost:
